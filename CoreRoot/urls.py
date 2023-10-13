7c1ee50559d12dbf42f65edc5f6e79d07707ae6c
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-13 17:28:56 +0545

    Integrating Rest Framework with Insomnia

--- CoreRoot/urls.py
+++ CoreRoot/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from core import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(routers.urlpatterns)),
 ]
