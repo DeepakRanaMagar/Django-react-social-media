f66ad258230bbd1d5244280d060ac9b0d8e6c88a
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-13 18:41:19 +0545

    JWT Auth

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -40,7 +40,18 @@ INSTALLED_APPS = [
     'core',
     'core.user',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ),
+    'DEFAULT_FILTER_BACKENDS':(
+        ['django_filters.rest_framework.DjangoFilterBackend'],
+        ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
