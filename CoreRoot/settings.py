31153b4fa3c7968fc48a6eb1c67ef43edc3c7fe9
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-14 08:32:14 +0545

    Init Post

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'core',
     'core.user',
     'core.auth',
+    'core.post',
+    
     'rest_framework',
     'rest_framework_simplejwt',
 ]
--- core/post/apps.py
+++ core/post/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'post'
+    name = 'core.post'
+    label = 'core_label'
