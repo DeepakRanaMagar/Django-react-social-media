fef81cfca4d221fc091f5ea7a12edc776ed8272a
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-15 10:31:35 +0545

    Installed Likes

---
+++ core/comment/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from core.abstract.models import AbstractModel, AbstractManager
+from core.post.models import Post
+from core.user.models import User
+
+
+
+class CommentManager(AbstractManager):
+    pass
+
+class Comment(AbstractModel):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    body = models.TextField()
+    edited = models.BooleanField(default=False)
+    
+    objects = CommentManager()
+    
+    def __str__(self):
+        return self.author.name
