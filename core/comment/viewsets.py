d07cd8bba93e803c80596bba900562b484021831
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-16 14:14:08 +0545

    Import Error

--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -13,3 +13,23 @@ class CommentViewSet(AbstractViewSet):
     permission_classes = (UserPermission,)
     serializer_class = CommentSerializer
     
+    def get_queryset(self):
+        if self.request.user.is_super:
+            return Comment.objects.all()
+        post_pk = self.kwargs('post_pk')
+        if post_pk is None:
+            return Http404
+        queryset = Comment.objects.filter(post__public_id=post_pk)
+        
+        return queryset
+    
+    def get_object(self):
+        obj = Comment.objects.get_object_by_public_id(self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+        return obj 
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- core/routers.py
+++ core/routers.py
@@ -1,10 +1,11 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from core.user.viewsets import UserViewSet
 from django.urls import path, include
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
 from core.post.viewsets import PostViewSet
+from core.comment.viewsets import CommentViewSet
 
 
 router = routers.SimpleRouter()
@@ -18,6 +19,11 @@ router.register(r'auth/login', LoginViewSet, basename = 'auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename = 'auth-refresh')
 router.register(r'post', PostViewSet, basename='post')
 
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
+posts_router.register(r'comment', CommentViewSet, basename='post-comment')
+
+
 urlpatterns = [
     *router.urls,
+    *posts_router.urls,
 ]
