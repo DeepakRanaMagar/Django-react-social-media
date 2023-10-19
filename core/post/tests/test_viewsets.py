02a4005f63c4385d8de3a86f4f78405665687b90
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-19 15:47:55 +0545

    .

---
+++ core/post/tests/test_viewsets.py
@@ -0,0 +1,45 @@
+from rest_framework import status
+from core.fixtures.user import user
+from core.fixtures.post import post
+
+class TestPostViewSet:
+    endpoint = '/api/post/'
+    
+    def test_list(self, client, user, post):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1 
+    
+    def test_retrieve(self, client, user, post):
+        client.force_authenticate(user = user) 
+        response = client.get(self.endpoint + str(post.public_id) +"/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == post.public_id.hex
+        assert response.data['body'] == post.body
+        assert response.daata['author']['id'] == post.author.public_id.hex
+        
+    def test_create(self, client, user):
+        client.force_authenticate(user = user)
+        data ={
+            "body":"test Post body",
+            "author":user.public_id.hex
+        }
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['author']['id']==user.public_id.hex
+        
+    def test_update(self, client, user, post):
+        client.force_authenticate(user = user)
+        data ={
+            "body":"Test Post Bpduy",
+            "author":user.public_id.hex
+        }
+        response = client.put(self.endpoint + str(post.public_id) + "/", data)
+        assert response.status_code == status.HTTP_200_OK 
+        assert response.data['body'] == data['body']
+        
+    def test_delete(self, client, user, post):
+        client.force_authenticate(user=user)
+        response = client.delete(self.endpoint + str(post.public_id)+ "/")
+        assert response.status_code == status.HTTP_204_NO_CONTENT
