4efbd7d799df22801f9a3a0e2171c74ff2d40c0f
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-22 08:12:18 +0545

    initial front end page

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello this is my first 
-        </a>
-      </header>
-    </div>
-  );
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    );
 }
 
 export default App;
---
+++ social-media-app/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <h1>
+                Profile
+            </h1>
+            <p>
+                Welcome!
+            </p>
+        </div>
+    );
+}
+export default Home;
