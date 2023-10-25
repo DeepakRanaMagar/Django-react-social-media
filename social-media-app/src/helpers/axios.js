3d77ba396889753d2b62ab023237f055c8b7dd50
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-25 21:03:38 +0545

    .

--- social-media-app/src/helpers/axios.js
+++ social-media-app/src/helpers/axios.js
@@ -14,3 +14,34 @@ axiosService.interceptors.request.use(async (config) =>{
     return config;
 });
 
+axiosService.interceptors.response.use(
+    (res)=>Promise.resolve(res),
+    (err)=>Promise.reject(err),
+);
+
+const refreshAuthLogic = async (failedRequest) => {
+    const { refresh } = JSON.parse(localStorage.getItem("auth"));
+    return axios
+        .post("/refresh/token/", null, {
+            baseURL: "http://localhost:8000",
+            headers: {
+                Authorization: `Bearer ${refresh}`,
+            },
+        })
+        .then((resp) => {
+            const { access, refresh } = resp.data;
+            failedRequest.response.config.headers["Authorization"] = `Bearer ${access}`;
+            localStorage.setItem("auth", JSON.stringify({ access, refresh }));
+        })
+        .catch(() => {
+            localStorage.removeItem("auth");
+        });
+};
+
+createAuthRefreshInterceptor(axiosService, refreshAuthLogic);
+
+export function fetcher(url){
+    return axiosService.get(url).then((res)=>res.data);
+}
+
+export default axiosService;
