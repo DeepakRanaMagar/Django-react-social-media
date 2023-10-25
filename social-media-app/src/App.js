1ba6ff67d9579f065847e0555d8e1c19b4587c38
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-25 21:14:36 +0545

    fix protectedroutes

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ProtectedRoute from './routes/ProtectedRoute';
 
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          
+          <ProtectedRoute>
+        
+            <Home />
+        
+          </ProtectedRoute>
+        
+        } />
+        <Route path="/login/" element={<div>Login</div>}/>
       </Routes>
     );
 }
---
+++ social-media-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute( { children }){
+    const { user } = JSON.parse(localStorage.getItem("auth"));
+    return auth.account ? <>{ children }</> : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
