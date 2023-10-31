707a82aee6da0c29b9f78c72b74cec496f03d8d1
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-10-31 07:57:16 +0545

    Hooks

--- social-media-app/src/hooks/user.actions.js
+++ social-media-app/src/hooks/user.actions.js
@@ -5,42 +5,45 @@ function useUserActions(){
     const navigate = useNavigate();
     const baseURL = "http://localhost:8000/api";
 
+    function login(data){
+        return axios.post(`${baseURL}/auth/login`,data).then((res) => {
+            // Registering the account and tokens in the store
+            setUserData(data);
+            navigate("/");
+        });
+    }
+    
+    function logout(){
+        localStorage.removeItem("auth");
+        navigate("/login");
+    }
+    
+    function getAccessToken(){
+        const auth = JSON.parse(localStorage.getItem("auth"));
+        return auth.access;
+    }
+    
+    function getRefreshToken(){
+        const auth = JSON.parse(localStorage.getItem("auth"));
+        return auth.refresh;
+    }
+    
+    function setUserData(data){ 
+        localStorage.setItem(
+            "auth",
+            JSON.stringify({
+                access: res.data.access,
+                refresh: res.data.refresh,
+                user: res.data.user,
+            })
+        );
+    }
+
     return(
         login,
         register,
         logout,
         );
 }
-function login(data){
-    return axios.post(`${baseURL}/auth/login`,data).then((res) => {
-        // Registering the account and tokens in the store
-        setUserData(data);
-        navigate("/");
-    });
-}
 
-function logout(){
-    localStorage.removeItem("auth");
-    navigate("/login");
-}
-
-function getAccessToken(){
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    return auth.access;
-}
-
-function getRefreshToken(){
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    return auth.refresh;
-}
-
-function setUserData(data){ 
-    localStorage.setItem(
-        "auth",
-        JSON.stringify({
-            access: res.data.access,
-            refresh: res.data.refresh,
-            user: res.data.user,
-        })
-    );
-}
+export default useUserActions;
