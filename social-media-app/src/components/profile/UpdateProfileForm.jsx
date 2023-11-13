4ddb7e508c24f0b8a81cc8362e81c8904485288d
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-11-13 14:40:54 +0545

    .

--- social-media-app/src/components/profile/UpdateProfileForm.jsx
+++ social-media-app/src/components/profile/UpdateProfileForm.jsx
@@ -44,10 +44,105 @@ function UpdateProfileForm(props){
             formData.append("avatar", avatar);
         }
 
+        userActions
+            .edit(formData, profile.id)
+            .then( ()=> {
+                setToaster({
+                    type: "success",
+                    message: "Profile updated successfully",
+                    show: true,
+                    title: "Updated",
+                });
+                navigate(-1);
+            })
+            .catch( (err)=> {
+                if(err.message){
+                    setError(err.request.response);       
+                }
+            });
         
     };
     return(
         // UI code
+        <Form id="registration-form" 
+            className="border p-4 rounded"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+        >
+            {/* Group for avatar  */}
+            <Form.Group
+                className="mb-3 d-flex flex-column">
+
+                <Form.Label className="text-center">Avatar</Form.Label>
+                <Image src={form.avatar} roundedCircle width={120} height={120} />
+                <Form.Control 
+                    onChange={ (e)=> setAvatar(e.target.files[0])}
+                    className="w-50 align-self-center"
+                    type="file"
+                    size="sm"
+                />
+                <Form.Control.Feedback type="invalid">
+                    This file is required.
+                </Form.Control.Feedback>
+
+            </Form.Group>
+
+
+            {/* Group for First Name */}
+            <Form.Group className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control 
+                    value={form.first_name}
+                    onChange={(e)=> setForm({
+                        ...form,
+                        first_name: e.target.value
+                    })}
+                    required
+                    type="text"
+                    placeholder="First Name"
+                />
+                <Form.Control.Feedback type="invalide">This file is required.</Form.Control.Feedback>
+            </Form.Group>
+            
+            {/* Group for Last Name */}
+            <Form.Group className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control 
+                    value={form.last_name}
+                    onChange={(e)=> setForm({
+                        ...form,
+                        last_name: e.target.value
+                    })}
+                    required
+                    type="text"
+                    placeholder="Last Name"
+                />
+                <Form.Control.Feedback type="invalide">This file is required.</Form.Control.Feedback>
+            </Form.Group>
+
+            {/* Group for Bio */}
+            <Form.Group className="mb-3">
+                <Form.Label>Bio</Form.Label>
+                <Form.Control 
+                    value={form.bio}
+                    onChange={(e)=> setForm({
+                        ...form,
+                        bio: e.target.value
+                    })}
+                    required
+                    type="textarea"
+                    placeholder="Write your Bio (Optional)"
+                    rows={3}
+                />
+            </Form.Group>
+            
+            <div className="text-content text-danger">{error && <p>{error}</p>}</div>
+            
+            <Button variant="primary" type="submit">
+                Save changes
+            </Button>
+        </Form>
     );
 }
 export default UpdateProfileForm;
---
+++ social-media-app/src/pages/EditProfile.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import { fetcher } from "../helpers/axios";
+import Layout from "../components/Layout";
+import { Row,Col } from "react-bootstrap";
+import UpdateProfileForm from "../components/profile/UpdateProfileForm";
+
+function EditProfile(){
+    
+    const { profileId } = useParams();
+
+    const profile = useSWR(`/user/${profileId}/`, fetcher);
+    
+    return(
+        <Layout hasNavigationBack>
+            {profile.data?(
+                <Row className="justify-content-evenly">
+                    <Col sm={9}>
+                        <UpdateProfileForm/>
+                    </Col>
+                </Row>
+            ):(
+                <div>Loading...</div>
+            )}
+        </Layout>
+    );
+}
+
+export default EditProfile;
